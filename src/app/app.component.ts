cfb6a67574596a25b5ba08bb06a080092349ac96
Bharat Ramnani <bharat.ramnani@hdfcred.com>
2017-11-21 16:14:41 +0530

    Add basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import domtoimage from 'dom-to-image';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+      
+    name:string = 'Bharat Ramnani';
+    designation:string = 'CTO';
+    phone:string = '975.237.5491';
+    place:string = 'Mumbai IN 400020';
+    mail:string = 'hi@bharatramnani.com';
+    color:any = '#ffffff';
+    background:any = '#0090ff';
+    borderRadius:string = '2';
+
+
+    private nodeIdForExport:string = 'js-card';
+
+    private saveAndOpen(dataUrl: any) {
+        var link = document.createElement('a');
+        link.download = this.name + ' Business Card';
+        link.href = dataUrl;
+        link.click();
+    }
+
+    generateCard(filetype:string) {
+
+        let nodeToExport = document.getElementById(this.nodeIdForExport);
+
+        switch (filetype) {
+            
+            case "svg":
+                debugger;
+                domtoimage.toSvg(nodeToExport)
+                    .then((dataUrl) => this.saveAndOpen(dataUrl));
+                break;
+                
+            default:
+                domtoimage.toJpeg(nodeToExport, { quality: 1 })
+                    .then((dataUrl) => this.saveAndOpen(dataUrl));
+                break;
+        }
+    }
+
+
 }
