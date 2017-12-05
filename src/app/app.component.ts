fc174fa52171fc080c638fcdf91f79b8eccf6282
Bharat Ramnani <bharatramnani94@gmail.com>
2017-12-05 19:27:05 +0530

    Add feature to generate constituency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,87 +2,72 @@ import { Component } from '@angular/core';
 import domtoimage from 'dom-to-image';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-      
-    name:string = 'Bharat Ramnani';
-    designation:string = 'CTO';
-    phone:string = '975.237.5491';
-    place:string = 'Mumbai IN 400020';
-    mail:string = 'hi@bharatramnani.com';
-    color:any = '#ffffff';
-    background:any = '#0090ff';
-    backgroundOpacity:string = '0.8';
-    patternOpacity:string = '1';
-    letterSpacing:string = '0.14';
-    pattern:string = './assets/patterns/pattern1.jpg';
-    borderRadius:string = '2';
-    alignment:string = 'left';  // left, center, right
-
-    patternsList:string[] = [
-        'pattern1.jpg',
-        'pattern2.gif'
-    ];
 
 
-    private nodeIdForExport:string = 'js-card';
+  COLOR: string = "red";
 
-    private saveAndOpen(dataUrl: any) {
-        var link = document.createElement('a');
-        link.download = this.name + ' Business Card';
-        link.href = dataUrl;
-        link.click();
+
+  ngOnInit() {
+    setTimeout(() => {
+      console.log("Starting...");
+      this.extractConstituencyImages();
+    }, 1000);
+  }
+
+
+  private nodeIdForExport: string = 'js-card';
+
+  private saveAndOpen(dataUrl: any, currentConstituency: number) {
+    var link = document.createElement('a');
+    link.download = 'gujarat-constituency-' + currentConstituency + '-' + this.COLOR.replace('#', '');
+    link.href = dataUrl;
+    link.click();
+    this.resetStyles(currentConstituency);
+  }
+
+  private resetStyles(currentConstituency: number) {
+    let el = document.getElementById('GUJARAT-' + currentConstituency);
+    el.style.fill = 'transparent';
+  }
+
+  generateCard(filetype: string = 'jpg', currentConstituency: number) {
+
+    let nodeToExport = document.getElementById(this.nodeIdForExport);
+
+    switch (filetype) {
+
+      case "svg":
+        domtoimage.toSvg(nodeToExport)
+          .then((dataUrl) => this.saveAndOpen(dataUrl, currentConstituency));
+        break;
+
+      case "jpg":
+        domtoimage.toJpeg(nodeToExport, { quality: 1 })
+          .then((dataUrl) => this.saveAndOpen(dataUrl, currentConstituency));
+        break;
+      case "png":
+      default:
+        domtoimage.toPng(nodeToExport, { quality: 1 })
+          .then((dataUrl) => this.saveAndOpen(dataUrl, currentConstituency));
+        break;
     }
+  }
 
-    generateCard(filetype:string = 'jpg') {
+  extractConstituencyImages(color: string = this.COLOR) {
+    for (let i = 1; i <= 182; i++) {
+      setTimeout(() => {
+        let e = document.getElementById('GUJARAT-' + i);
+        e.style.fill = color;
 
-        let nodeToExport = document.getElementById(this.nodeIdForExport);
-
-        switch (filetype) {
-            
-            case "svg":
-                domtoimage.toSvg(nodeToExport)
-                    .then((dataUrl) => this.saveAndOpen(dataUrl));
-                break;
-            
-            case "jpg":
-                domtoimage.toJpeg(nodeToExport, { quality: 1 })
-                    .then((dataUrl) => this.saveAndOpen(dataUrl));
-                break;
-            case "png":
-            default:
-                domtoimage.toPng(nodeToExport, { quality: 1 })
-                    .then((dataUrl) => this.saveAndOpen(dataUrl));
-                break;
-        }
+        this.generateCard('png', i);
+      }, 3000 + (3000 * i));
     }
-
-    patternChanged(patternName:string) {
-        this.pattern = './assets/patterns/' + patternName;
-    }
-
-    alignmentChanged(newAlignment:string = 'left') {
-        this.alignment = newAlignment;
-    }
-
-    customBackgroundChanged(event:any) {
-        let file = event.target.files[0];
-        this.readFile(file);
-    }
-
-    private readFile(file:any) {
-        let reader = new FileReader();
-        reader.onload = (event) => {
-            this.pattern = event.target["result"];
-        };
-        reader.onerror = (event) => {
-            alert("File could not be read! Code " + event.target["error"].code);
-        };
-        reader.readAsDataURL(file);
-    }
+  }
 
 
 }
